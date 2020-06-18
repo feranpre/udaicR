999946b4a4584dc7408cefa7409f6575fd9a678c
feranpre <feranpre@gmail.com>
2020-06-18 20:11:39 +0200

    Added fecha.spss.R

--- R/fecha.spss.R
+++ R/fecha.spss.R
@@ -1,7 +1,8 @@
 #Para obtener la fecha con horas minutos y segundos
 pss2datetime <- function(x) as.POSIXct(x, origin = "1582-10-14", tz="+1")
-
+spss.date.time <- pss2datetime
 
 #Para obtener la fecha sin hora
 
 pss2date <- function(x) as.Date(x/86400, origin = "1582-10-14")
+spss.date <- pss2date
