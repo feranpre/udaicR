88c526674457e8a488bb473b727cf534624de3e1
feranpre <feranpre@gmail.com>
2020-07-26 16:46:50 +0200

    añadidas las funciones para  transformar fechas que vienen de SPSS

--- R/fecha.spss.R
+++ R/fecha.spss.R
@@ -1,8 +1,48 @@
-#Para obtener la fecha con horas minutos y segundos
-pss2datetime <- function(x) as.POSIXct(x, origin = "1582-10-14", tz="+1")
+#' Title pss2datetime
+#'
+#' Para obtener la fecha con horas minutos y segundos
+#' @param x   Fecha en formato SPSS con HORA
+#'
+#' @return    Fecha en formato R
+#' @export
+#'
+#' @examples
+pss2datetime <- function(x) {
+  return(as.POSIXct(x, origin = "1582-10-14", tz="+1"))
+}
+
+#' Title spss.date.time
+#'
+#' Para obtener la fecha con horas minutos y segundos
+#' @param x   Fecha en formato SPSS con HORA
+#'
+#' @return    Fecha en formato R
+#' @export
+#'
+#' @examples
 spss.date.time <- pss2datetime
 
-#Para obtener la fecha sin hora
 
-pss2date <- function(x) as.Date(x/86400, origin = "1582-10-14")
+#' Title pss2date
+#'
+#' Para obtener la fecha
+#' @param x   Fecha en formato SPSS sin HORA
+#'
+#' @return    Fecha en formato R
+#' @export
+#'
+#' @examples
+pss2date <- function(x) {
+  return(as.Date(x/86400, origin = "1582-10-14"))
+}
+
+#' Title spss.date
+#'
+#' Para obtener la fecha
+#' @param x   Fecha en formato SPSS sin HORA
+#'
+#' @return    Fecha en formato R
+#' @export
+#'
+#' @examples
 spss.date <- pss2date
